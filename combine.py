bceb7be2449591d7a4178887b5f38b04bd4a7afb
PavKmiec <pawelkmiec07@gmail.com>
2019-08-30 21:36:47 +0100

    c

--- combine.py
+++ combine.py
@@ -26,14 +26,6 @@ def createDict():
 
 createDict()
 
-# def loadImages():
-#     for filename in fileFolder:
-#         im=Image.open(filename)
-#         image_list.append(im)
-#     return image_list
-
-# loadImages()
-
 
 for key, value in data.items():
     print(key, value)
@@ -41,13 +33,6 @@ for key, value in data.items():
 
 print('---------------------------------------------')
 
-
-# #for each image in list pass in image url with image name to the response 
-# for image in image_list:
-#     response = model.predict_by_filename(image.filename)
-#     print(image)
-#     for r in response['outputs'][0]['data']['concepts']: # for each element in response print key-words
-#         print(r['name'])
 
 
 for key, value in data.items():
@@ -58,32 +43,6 @@ for key, value in data.items():
                 print(r['name']) # for each element in response print key-words
 
 
-
+# check data dict
 for key, value in data.items():
     print(key, value)
-
-        # new_data = {item: ['']}
-        # data[item] = new_data[item]
-
-
-# import list of year,value pairs
-
-# for year,value in mylist:
-#     try:
-#         d[year].append(value)
-#     except KeyError:
-#         d[year] = [value]
-
-
-
-
-
-
-
-
-        # response = model.predict_by_filename(inageUrl)
-        # for r in response['outputs'][0]['data']['concepts']:
-        #         print(inageUrl)
-        #         print(r['name']) # for each element in response print key-words
-
-
