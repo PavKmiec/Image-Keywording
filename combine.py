c1aa758fdb4edbab5d9ad0f9c86e04baa7c733d3
PavKmiec <pawelkmiec07@gmail.com>
2019-08-31 02:55:11 +0100

    keywords appending working

--- combine.py
+++ combine.py
@@ -3,20 +3,17 @@ from clarifai.rest import ClarifaiApp
 from PIL import Image
 import glob
 from iptcinfo3 import IPTCInfo
+
 # instance of Clarifai App - passing in api key
-app = ClarifaiApp(api_key='KEY HERE')
+app = ClarifaiApp(api_key='8347755eb1694ac8a447cdec8261a9db')
 # selection of pre-trained model (for image recognition)
 model = app.public_models.general_model
-
-# list vaiable for storing image locations
-image_list = []
-
 
 # folder containing multiple image files
 fileFolder = glob.glob('./street/*.*')
 
 
-
+# dictionary to keep file path and keywords
 data = {}
 
 def createDict():
@@ -27,14 +24,14 @@ def createDict():
 createDict()
 
 
-for key, value in data.items():
-    print(key, value)
+# for key, value in data.items():
+#     print(key, value)
 
 
 print('---------------------------------------------')
 
 
-
+# loop data dictionary 
 for key, value in data.items():
         print(key)
         response = model.predict_by_filename(key)
@@ -42,7 +39,32 @@ for key, value in data.items():
                 data[key].append(r['name'])
                 print(r['name']) # for each element in response print key-words
 
+# # check data dict
+# for key, value in data.items():
+#     print(key, value)
 
-# check data dict
+
+
+# take the values in dict, match key value to a file path and append IPTC keywords to file
 for key, value in data.items():
-    print(key, value)
+        #print(os.path.basename(key))
+         info = IPTCInfo(os.path.basename(key))
+         for item in value:
+                 newValue = item
+                 info['keywords'].append(newValue)
+         info.save()
+
+#info.save()
+
+
+
+
+
+# newKeywords = {'key2', 'key2', 'key3'}
+
+# for key in newKeywords:
+# 	info['keywords'].append(key)
+
+
+# info.save()
+# print(info['keywords'])
