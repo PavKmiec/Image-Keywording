8f8c711835ad9398e08b8c5edd6358d205cac25e
PavKmiec <pawelkmiec@gmail.com>
2020-07-26 12:40:50 +0100

    added comments

--- combine.py
+++ combine.py
@@ -3,19 +3,21 @@ from clarifai.rest import ClarifaiApp
 from PIL import Image
 import glob
 from iptcinfo3 import IPTCInfo
+import cred
 
 # instance of Clarifai App - passing in api key
-app = ClarifaiApp(api_key='8347755eb1694ac8a447cdec8261a9db')
+app = ClarifaiApp(api_key=cred.key)
 # selection of pre-trained model (for image recognition)
 model = app.public_models.general_model
 
 # folder containing multiple image files
-fileFolder = glob.glob('./images/*.jpg')
+fileFolder = glob.glob('/home/px/Desktop/repos/keywording/IPTC-Keywording/images/*.jpg')
 
 
 # dictionary to keep file path and keywords
 data = {}
 
+# create dictionary for storing file path as key and array of keywords as value 
 def createDict():
     for item in fileFolder:
         new_data = {item: []}
@@ -23,32 +25,28 @@ def createDict():
 
 createDict()
 
-# for key, value in data.items():
-#     print(key, value)
-def resize(photo):
-        basewidth = 300
-        img = Image.open(photo)
-        wpercent = (basewidth/float(img.size[0]))
-        hsize = int((float(img.size[1])*float(wpercent)))
-        #img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-        out = img.resize((basewidth,hsize), Image.ANTIALIAS)
-        out.save()
-        return img.filename # TODO return image path instaed of image !!
-
 print('---------------------------------------------')
 
 
-# loop data dictionary 
+
+# loop data dictionary
+#  
 for key, value in data.items():
         print(key)
-        response = model.predict_by_filename(key) # get image path from resize method TODO
+        print(value)
+
+        # set recognition model
+        response = model.predict_by_filename(key) # TODO get image path from resize method TODO
+
+        # append generated keywords in dictionary
         for r in response['outputs'][0]['data']['concepts']:
                 data[key].append(r['name'])
                 print(r['name']) # for each element in response print key-words
 
 
 
-# take the values in dict, match key value to a file path and append IPTC keywords to file
+# take the values from dictionary, 
+# match key value to a file path and append IPTC keywords to image file
 for key, value in data.items():
         #print(os.path.basename(key))
          info = IPTCInfo(key)
@@ -56,3 +54,21 @@ for key, value in data.items():
                  newValue = item
                  info['keywords'].append(newValue)
          info.save()
+
+#--------------------------------------------------------------------#
+
+## TODO
+
+## Implement image resizing method to improve performance
+###  Keywords MUST be saved back to Oryginal (not resized images)
+
+# # method to resize
+# def resize(photo):
+#         basewidth = 300
+#         img = Image.open(photo)
+#         wpercent = (basewidth/float(img.size[0]))
+#         hsize = int((float(img.size[1])*float(wpercent)))
+#         #img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+#         out = img.resize((basewidth,hsize), Image.ANTIALIAS)
+#         out.save()
+#         return img.filename # TODO return image path instaed of image !!
