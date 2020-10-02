b7b58156710092ce721b1bad48f051ee4a8316a9
saiharsha-22 <harshavardhangood@gmail.com>
2020-10-02 13:40:52 +0530

    Image resize

--- combine.py
+++ combine.py
@@ -1,12 +1,16 @@
 import os
+import PIL
+from PIL import Image
 from clarifai.rest import ClarifaiApp
 from PIL import Image
 import glob
 from iptcinfo3 import IPTCInfo
 import cred
 
+
+
 # instance of Clarifai App - passing in api key
-app = ClarifaiApp(api_key=cred.key)
+app = ClarifaiApp(api_key= cred.key)
 # selection of pre-trained model (for image recognition)
 model = app.public_models.general_model
 
@@ -48,7 +52,7 @@ for key, value in data.items():
 # take the values from dictionary, 
 # match key value to a file path and append IPTC keywords to image file
 for key, value in data.items():
-        #print(os.path.basename(key))
+         print(os.path.basename(key))
          info = IPTCInfo(key)
          for item in value:
                  newValue = item
@@ -63,12 +67,14 @@ for key, value in data.items():
 ###  Keywords MUST be saved back to Oryginal (not resized images)
 
 # # method to resize
-# def resize(photo):
-#         basewidth = 300
-#         img = Image.open(photo)
-#         wpercent = (basewidth/float(img.size[0]))
-#         hsize = int((float(img.size[1])*float(wpercent)))
-#         #img = img.resize((basewidth,hsize), Image.ANTIALIAS)
-#         out = img.resize((basewidth,hsize), Image.ANTIALIAS)
-#         out.save()
-#         return img.filename # TODO return image path instaed of image !!
+
+def resize(photo):
+        baseheight = 560
+        img = Image.open(photo)
+        hpercent = (baseheight / float(img.size[1])) 
+        wsize = int((float(img.size[1]) * float(hpercent)))
+        img = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
+        out = img.resize((wsize, baseheight), PIL.Image.ANTIALIAS)
+        resized_image = str(photo.split('.')[0]+'_resized.'+photo.split('.')[1])
+        out.save(resized_image)
+        return resized_image # TODO return image path instaed of image !!
